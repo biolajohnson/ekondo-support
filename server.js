4f2e1c6cd66b4c2637ea078ab4868f632aa0e116
Abiola Johnson <abiolajohnson@Abiolas-MacBook-Pro.local>
2021-03-07 11:23:37 +0100

    serve

--- server.js
+++ server.js
@@ -15,14 +15,13 @@ app.use(express.json());
 dotenv.config();
 connectDB();
 const port = process.env.PORT;
-const __dirname__ = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("public"));
-  app.use(express.static(path.join(__dirname__, "frontend", "build")));
+  app.use(express.static(path.join(__dirname, "frontend", "build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname__, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
   });
-  app.use("/uploads", express.static(path.join(__dirname__, "/uploads")));
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 }
 
 app.use("/api/complaint", complaintRoutes);
